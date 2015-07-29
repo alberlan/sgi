196cc519883a6683b5a323cb9465b6189bf94056
PROGRAMADOR <PROGRAMADOR@PROGRAMADOR>
2015-07-29 17:23:34 -0400

    Criando EntityManager

---
+++ sgi/src/main/java/com/sgi/repository/GrupoProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.sgi.repository;
+
+import java.io.Serializable;
+
+public class GrupoProdutoRepository implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+}
